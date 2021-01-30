d71409685ff4f3985a891e27b194426ff52d48a9
Voregsele <noid379@gmail.com>
2021-01-30 23:52:46 +0400

    a2

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Home_work
 {
+    class Point
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        public Point(){}
+        public Point(int X,int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
